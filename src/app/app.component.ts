755d1cc8090f36f69829e5f03c0bf960d78649a9
Yordy Almonte <yoalmonte.dev@outlook.com>
2024-07-28 21:34:38 -0400

    👾 Task Model created, updated changeHandler() & addTask() added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Task } from './models/task.model';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,18 +11,44 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  tasks = signal([
-    'Instalar Angular CLI',
-    'Crear Proyecto',
-    'Crear Componente',
-    'Crear Servicio'
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Instalar Angular CLI',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Crear Proyecto',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Crear Componente',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Crear Servicio',
+      completed: false
+    },
   ]);
+
+  addTask(title: string) {
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed: false
+    };
+    this.tasks.update((tasks) => [...tasks, newTask])
+  }
 
   changeHandler(event: Event) {
     const input = event.target as HTMLInputElement;
     const newTask = input.value;
-    this.tasks.update((tasks) => [...tasks, newTask])
+    this.addTask(newTask)
     input.value = ''
   };
 
